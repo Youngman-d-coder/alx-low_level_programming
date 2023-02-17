75359f559f39fb9921767c09b56863a9cfc20520
nelson <sayhitonelson@gmail.com>
2023-02-17 00:06:29 -0800

    I am tired, nothing I do seem to work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,16 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Error)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = 59;
+	size_t bytes_written = 0;
+	while (bytes_written < len)
+	{
+		bytes_written += write(1, msg + bytes_written, len - bytes_written);
+	}
 	return (1);
 }
