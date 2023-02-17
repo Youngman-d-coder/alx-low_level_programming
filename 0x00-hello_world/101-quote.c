59d4e1c3b3ae10d24cc7d7f060d78792d80302e0
nelson <sayhitonelson@gmail.com>
2023-02-17 01:02:24 -0800

    lord help me!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,16 +4,12 @@
 /**
  * main - Entry point
  *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char *msg = 
-		("and that piece of art is useful\" - Dora Korpar, 2016-10-19");
-	
-	while (size_t 0 < size_t 59)
-	{
-		bytes_written += write(1, msg + bytes_written, len - bytes_written);
-	}
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
