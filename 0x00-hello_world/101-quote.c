14c4a81b673fd1be1931713bcdda935c0a95070f
nelson <sayhitonelson@gmail.com>
2023-02-16 19:05:58 -0800

    I had to start reading new materials to learn how to print without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard error.
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+	ssize_t bytes_written = 0;
+	while (bytes_written < len)
+	{
+		bytes_written += write(2, msg + bytes_written, len - bytes_written);
+	}
+	return 1;
+}
