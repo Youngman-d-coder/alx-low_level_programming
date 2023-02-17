4482d8b0f2fcc876df935b5d4af27965466a21e8
nelson <sayhitonelson@gmail.com>
2023-02-16 23:09:42 -0800

    My brains hurt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,20 +3,24 @@
 /**
  * main - Entry point
  *
- * Description: Prints a message to the standard error.
- *
- * Return: 1
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
+	ssize_t bytes_written = 0;
 	char *msg = "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
-	ssize_t bytes_written = 0;
-
+	size_t len = 0;
+	
+	while (msg[len] != '\0')
+	{
+		len++;
+	}
+	
 	while (bytes_written < len)
 	{
 		bytes_written += write(2, msg + bytes_written, len - bytes_written);
 	}
 	return (1);
 }
+
