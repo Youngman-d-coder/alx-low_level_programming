06cf619835d1b56da34116eef6d2a19d35bb1653
nelson <sayhitonelson@gmail.com>
2023-02-16 19:21:22 -0800

    I had to start reading new materials to learn how to print without using printf or puts(I have never had to use this much of my brain and i still do not get it, but i can do it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,14 @@
  */
 int main(void)
 {
-	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *msg = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
 	ssize_t bytes_written = 0;
+
 	while (bytes_written < len)
 	{
 		bytes_written += write(2, msg + bytes_written, len - bytes_written);
 	}
-	return 1;
+	return (1);
 }
