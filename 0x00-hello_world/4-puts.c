7e0c12203692df73b414acd6f15c9043cb86ce51
nelson <sayhitonelson@gmail.com>
2023-02-16 18:08:35 -0800

    using puts() instead of printf()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@ int main(void)
 	/**
 	 *  main - Using puts instead of printf
 	 */
-	puts("\"Programming is like building a multilingual puzzle")	
+	puts("\"Programming is like building a multilingual puzzle");
 	/**
 	 * return -  returns 0
 	 */
