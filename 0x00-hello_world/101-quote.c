3c6c2ecd0383fefe140eeaee2e9ee8e81264fc63
nelson <sayhitonelson@gmail.com>
2023-02-17 00:44:07 -0800

    please work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,11 +9,9 @@
 int main(void)
 {
 	const char *msg = 
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t len = 59;
-	size_t bytes_written = 0;
+		("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
 	
-	while (bytes_written < len)
+	while (size_t 0 < size_t 59)
 	{
 		bytes_written += write(1, msg + bytes_written, len - bytes_written);
 	}
