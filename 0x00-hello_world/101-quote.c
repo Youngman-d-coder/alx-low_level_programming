6aa2025a0bfd2f8a16c1feb6d30c98f0f91d13aa
nelson <sayhitonelson@gmail.com>
2023-02-17 00:15:06 -0800

    please work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,11 @@
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *msg = 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = 59;
 	size_t bytes_written = 0;
+	
 	while (bytes_written < len)
 	{
 		bytes_written += write(1, msg + bytes_written, len - bytes_written);
