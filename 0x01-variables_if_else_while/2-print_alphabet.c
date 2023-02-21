283a99c2b0c2b0b7653f064fbe01e1a60a257bb5
nelson <sayhitonelson@gmail.com>
2023-02-20 21:51:46 -0800

    C++

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints the alphabets in small letters
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int C;
+	for (C = 97; C <= 122; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
