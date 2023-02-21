72071d8e8fd076cc99a178866c1e1fa6b3303c0e
nelson <sayhitonelson@gmail.com>
2023-02-20 22:12:26 -0800

    a - Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Main entry
+ *
+ * Description: prints the alphabets
+ *
+ * Return: returns 0 (success)
+ *
+ */
+int main(void)
+{
+	int C;
+	
+	for (C = 97; C <= 122; C++)
+	{
+		putchar(C);
+	}
+
+	for (C = 65; C <= 90; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
