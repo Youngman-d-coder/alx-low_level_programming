2d71a90c4c80f33b23890e8ff3e71e9754ea0d5d
nelson <sayhitonelson@gmail.com>
2023-02-23 14:08:17 -0800

    Debugging on a rush

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry function
+ * Description: Tests if Positive or Negative
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int C;
+
+	C = 0;
+	positive_or_negative(C);
+	return (0);
+}
