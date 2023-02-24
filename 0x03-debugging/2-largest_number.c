4672317c06a1c1527c2d07228f5ebe9495e51a68
nelson <sayhitonelson@gmail.com>
2023-02-23 20:18:04 -0800

    stress

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,25 +9,26 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if(c > b && b > a)
-    {
-	    largest = c
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
