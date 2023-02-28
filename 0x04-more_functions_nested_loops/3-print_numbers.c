c046001802594130309348510a1e8f189526726e
nelson <sayhitonelson@gmail.com>
2023-02-28 11:17:49 -0800

    help me

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: always 0(success)
  */
 
 void print_numbers(void)
@@ -13,4 +15,6 @@ void print_numbers(void)
         _putchar(c);
     }
     _putchar('\n');
+
+    return (0);
 }
