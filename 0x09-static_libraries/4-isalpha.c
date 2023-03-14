f017cc1793684e59fca41ba444bd321e7906ea40
nelson <sayhitonelson@gmail.com>
2023-03-14 06:10:17 -0700

    three days on this task

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
-		
+
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - checks alphabts
+ * @c: does the confirmation
+ * Return: 1 if c is an alpha, 0 if not
  */
 int _isalpha(int c)
 {
