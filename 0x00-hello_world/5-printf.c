2e0cb648a0a30c602356c57cde922a24aace214a
Nelson <sayhitonelson@gmail.com>
2023-05-19 04:17:05 +0100

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Description: prints a string using "printf"
+ *
+ * Return: always zero (0) on success
+ */
+int main(void)
+{
+	char sent[60];
+
+	sent = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", sent);
+
+	return (0);
+}
