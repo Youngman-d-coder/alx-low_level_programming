b416b787e3b4df76f8834c41d99c4ddb291c6c9b
Nelson <sayhitonelson@gmail.com>
2023-05-19 05:56:12 +0100

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - entry point of program
@@ -11,7 +12,7 @@ int main(void)
 {
 	char sent[60];
 
-	sent = "with proper grammar, but the outcome is a piece of art,";
+	strcpy(sent, "with proper grammar, but the outcome is a piece of art,");
 
 	printf("%s\n", sent);
 
