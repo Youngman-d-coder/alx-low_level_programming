23aa9236d880a5d4270414a533eae20cd5d5e7c1
Nelson <sayhitonelson@gmail.com>
2023-05-19 04:53:55 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,17 +4,12 @@
 /**
  * main - Entry point for program
  *
- * @str: Quote by Dora Korpar
  * Description: prints string without using "printf","puts" or relatives
  * Return: standard error one (1)
  */
 int main(void)
 {
-	char str[70];
-
-	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
-
-	write(str, strlen(str));
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (0);
 }
