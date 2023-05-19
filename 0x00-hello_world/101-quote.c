352fdb72fd2f28259d91a93a52c4678603e3e3f5
Nelson <sayhitonelson@gmail.com>
2023-05-19 04:55:46 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 
-	return (0);
+	return (1);
 }
