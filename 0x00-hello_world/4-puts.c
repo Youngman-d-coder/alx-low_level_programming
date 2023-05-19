b4bdcc50cb7b3eeafbedb39862a35a40032e10bc
Nelson <sayhitonelson@gmail.com>
2023-05-19 04:09:36 +0100

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: This program prints a string
+ *
+ * Return: on success returns 0
+ */
+int main(void)
+{
+	puts('"Programming is like building a multilingual puzzle');
+
+	return (0);
+}
