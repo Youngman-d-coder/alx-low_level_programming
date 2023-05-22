a751a813c8325f959c53fb83bac943b744ec004e
Nelson <sayhitonelson@gmail.com>
2023-05-22 07:06:03 +0100

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,17 @@
  */
 int main(void)
 {
-        int alpha;
+	int alpha;
 
-        for (alpha = 97; alpha <= 122; alpha++)
-        {
-                putchar(alpha);
-        }
+	for (alpha = 97; alpha <= 122; alpha++)
+	{
+		putchar(alpha);
+	}
 	for (alpha = 65; alpha <= 90; alpha++)
 	{
 		putchar(alpha);
 	}
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
-
