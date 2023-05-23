5d11801a5ccd126616698c36b5863c08ee6d6741
Nelson <sayhitonelson@gmail.com>
2023-05-23 20:35:48 +0100

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,13 +10,5 @@
  */
 int _isalpha(int c)
 {
-
-        if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	return((c >= 'a' && c <= ''z')||(c >= 'A' && c <= 'Z'));
 }
