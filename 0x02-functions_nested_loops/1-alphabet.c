08f4ca98e81dd6da64e5730d9cd343bd82662456
Nelson <sayhitonelson@gmail.com>
2023-05-23 19:22:50 +0100

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * @alpha: contains alphabet characters
+ *
+ * Description: Prints the alphabets
+ *
+ * Return: zero on success
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = a; alpha <= z; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+
+	return (0);
+}
