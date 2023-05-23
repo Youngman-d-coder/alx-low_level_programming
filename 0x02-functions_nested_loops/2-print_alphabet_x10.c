2a27e0ccb47ab4b73793a1c33092833f751e1536
Nelson <sayhitonelson@gmail.com>
2023-05-23 19:39:08 +0100

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,18 @@
  */
 void print_alphabet_x10(void)
 {
-        char alpha;
+	char alpha;
 	int count;
 
 	count = 0;
 	while (count <= 9)
 	{
-        	for (alpha = 'a'; alpha <= 'z'; alpha++)
-        	{
-                	_putchar(alpha);
-        	}
-        	_putchar('\n');
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
 
 		count++;
 	}
 }
-
