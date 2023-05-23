fa9076c3c5488bc253c9d0352c8c8db50ead0842
Nelson <sayhitonelson@gmail.com>
2023-05-23 21:17:57 +0100

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - code starts here
+ *
+ * @a: number to be checked
+ *
+ * Description: checks for absolute numbers
+ *
+ * Return: 1 if absolute and 0 if otherwise
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	return (-a);
+}
