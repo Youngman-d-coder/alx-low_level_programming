2357b73fa5e54032afc0a2b60872da227cf5273c
Nelson <sayhitonelson@gmail.com>
2023-05-23 20:13:56 +0100

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - important program
+ *
+ * Description: checks if alpha is lower
+ *
+ * Return: 1 if lower and zero if otherwise
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
