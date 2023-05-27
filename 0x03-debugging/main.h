651df8157f859ea229d6d55ca1b38713c9f8c86d
Nelson <sayhitonelson@gmail.com>
2023-05-27 09:43:30 +0100

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Libraries */
 #include <stdio.h>
 
+/* Prototype */
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+
 
 #endif /* MAIN_H */
