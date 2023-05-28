e5996c527ef29886aec1c8e319acebb82e4f28aa
Nelson <sayhitonelson@gmail.com>
2023-05-28 17:33:34 +0100

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,23 @@ int main(void)
 
 	for (num = 1; num < tot; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && num % 5 != 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (num % 5 == 0)
+		else if (num % 5 == 0 && num % 3 != 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if ((num % 3 == 0) && (num % 5 == 0))
+		else if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d ", num);
 		}
 	}
+	printf("\n");
+	return (0);
 }
