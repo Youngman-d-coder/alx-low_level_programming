b2c09e2577d6467f10061e66dc64272dec233022
Nelson <sayhitonelson@gmail.com>
2023-06-06 15:33:09 +0100

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of memory ptd by s with the const b
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Prototypes */
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
+#endif /* MAIN_H */
