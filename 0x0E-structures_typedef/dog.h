d66d9f701e33360cea99ab748266c259e52cae1d
Nelson <sayhitonelson@gmail.com>
2023-06-19 14:36:24 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - dog basic info
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: owner name
+ */
 struct dog{
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif /* DOG_H */
