a4dcb12551b5335b59d6db193f6d6ef9a785a90a
Nelson <sayhitonelson@gmail.com>
2023-06-22 14:56:31 +0100

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array
+ *
+ * @array: array to be iterated through
+ *
+ * size: size of array
+ *
+ * @action: function to be called
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,6 +7,7 @@
 
 /* Portfolio */
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 
