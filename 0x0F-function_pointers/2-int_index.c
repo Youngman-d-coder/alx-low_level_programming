7d78aa9f06a3fb252cf454c922b1c228fb311f7c
Nelson <sayhitonelson@gmail.com>
2023-06-22 15:50:26 +0100

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL && size <= 0 && cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
