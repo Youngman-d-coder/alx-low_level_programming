e3739add44260899fbcf9c4f97f0269ae38ec018
Nelson <sayhitonelson@gmail.com>
2023-06-24 04:07:35 +0100

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,7 +2,7 @@
 
 /**
  * get_op_func - chooses the correct function to perfor the perform
- * 		the operation by user preference
+ *		the operation by user preference
  *
  * @s: operator passed as argument
  *
@@ -11,26 +11,26 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(ops[i].op, s) == 0)
-        {
-            return ops[i].f;
-        }
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
 
-	i++;
-    }
+		i++;
+	}
 
-    return NULL;
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *op;
+	int num1, num2, result;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,23 +21,24 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	result = func(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
