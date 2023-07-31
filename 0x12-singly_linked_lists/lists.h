e530c351f60df8a74c34d488a51f49c53433be22
Nelson <sayhitonelson@gmail.com>
2023-07-31 10:47:36 +0100

    deleting bit manipulation

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* STRUCTURES */
+
+/* PROTOTYPES */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
