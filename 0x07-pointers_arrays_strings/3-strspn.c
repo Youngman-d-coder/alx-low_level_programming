a0d24f4f6fae4134561543cf36b3dc566c577599
Nelson <sayhitonelson@gmail.com>
2024-01-16 19:26:58 +0100

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 
-	while (*s != '\0' && _strchr(accept, *s) != NULL)
+	while (*s != '\0' && strchr(accept, *s) != NULL)
 	{
 		count++;
 		s++;
