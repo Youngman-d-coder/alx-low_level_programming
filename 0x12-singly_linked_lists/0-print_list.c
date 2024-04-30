28f538a785088a6c0af6a0ed2eb46644a7427b6d
nelson <sayhitonelson@gmail.com>
2024-04-30 11:04:46 -0700

    update!

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,51 @@
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	char *str;
+	int len;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+			_putchar('\n');
+		}
+		else
+		{
+			len = h->len;
+			str = h->str;
+			_putchar('[');
+			while (len / 10)
+			{
+				len /= 10;
+			}
+			_putchar(len + '0');
+			_putchar(']');
+			_putchar(' ');
+			while (*str)
+			{
+				_putchar(*str);
+				str++;
+			}
+			_putchar('\n');
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
