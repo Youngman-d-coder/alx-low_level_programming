7e3020e621db3b543849fd98e5f8d73e2cd229a4
nelson <sayhitonelson@gmail.com>
2024-04-30 21:50:51 -0700

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
+st_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
